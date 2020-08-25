063490e990a3492ac0ec5badc2fcd05e8e1b3f45
Mohit Bansal <bansalmohit72@gmail.com>
2020-08-25 19:04:19 +0530

    Implemented subtract n days, weeks, months and years from given date and its unit test cases

---
+++ Week2AssignmentDateTimeCalculator/date-time-calculator-with-hibernate/src/main/java/com/sapient/datetimecalculator/operations/SubtractNFromGivenDate.java
@@ -0,0 +1,26 @@
+package com.sapient.datetimecalculator.operations;
+
+import java.time.LocalDateTime;
+
+import com.sapient.datetimecalculator.dbms.CalculatorInput;
+import com.sapient.datetimecalculator.dbms.CalculatorOutput;
+import com.sapient.datetimecalculator.dbms.NumericalDate;
+
+public class SubtractNFromGivenDate implements CalculatorOperation {
+
+	@Override
+	public CalculatorOutput perform(CalculatorInput inp) {
+		LocalDateTime date1 = inp.getDate1();
+		NumericalDate date3 = inp.getDate3();
+
+		LocalDateTime dateBeforNYears = date1.minusYears(date3.getNumberOfYears());
+		LocalDateTime dateBeforeNMonths = dateBeforNYears.minusMonths(date3.getNumberOfMonths());
+		LocalDateTime dateBeforeNWeeks = dateBeforeNMonths.minusWeeks(date3.getNumberOfWeeks());
+		LocalDateTime result = dateBeforeNWeeks.minusDays(date3.getNumberOfDays());
+
+		CalculatorOutput out = new CalculatorOutput();
+		out.setDate(result);
+		return out;
+	}
+
+}
