bb4ac1af63640187e939b544d43a6d1665a00e3c
Mohit Bansal <bansalmohit72@gmail.com>
2020-08-25 18:07:28 +0530

    Implemented calculator output pojo

---
+++ Week2AssignmentDateTimeCalculator/date-time-calculator-with-hibernate/src/main/java/com/sapient/datetimecalculator/dbms/CalculatorOutput.java
@@ -0,0 +1,76 @@
+package com.sapient.datetimecalculator.dbms;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class CalculatorOutput {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	private LocalDateTime date;
+	private NumericalDate numericalDate;
+
+	private String dayOfWeek;
+	private Integer weekNumber;
+
+	public CalculatorOutput() {
+		date = null;
+		numericalDate = null;
+		dayOfWeek = "";
+		weekNumber = 0;
+	}
+
+	public CalculatorOutput(LocalDateTime date, NumericalDate numericalDate, String dayOfWeek, Integer weekNumber) {
+		this.date = date;
+		this.numericalDate = numericalDate;
+		this.dayOfWeek = dayOfWeek;
+		this.weekNumber = weekNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public NumericalDate getNumericalDate() {
+		return numericalDate;
+	}
+
+	public void setNumericalDate(NumericalDate numericalDate) {
+		this.numericalDate = numericalDate;
+	}
+
+	public String getDayOfWeek() {
+		return dayOfWeek;
+	}
+
+	public void setDayOfWeek(String dayOfWeek) {
+		this.dayOfWeek = dayOfWeek;
+	}
+
+	public Integer getWeekNumber() {
+		return weekNumber;
+	}
+
+	public void setWeekNumber(Integer weekNumber) {
+		this.weekNumber = weekNumber;
+	}
+
+}
